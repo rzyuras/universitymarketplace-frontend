283416e8466795288458fd2bbac9dacbc6088360
Radoslav Yuras <rzyuras@uc.cl>
2024-10-13 23:08:52 -0300

    feat(Hello World): Se agrega component de WebSocket que prueba conexión al backend.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ItemList from './components/ItemList';
+import WebSocketComponent  from './components/WebSocketComponent';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <h1>University Marketplace UC</h1>
       </div>
+      <WebSocketComponent />
     </div>
   );
 }
